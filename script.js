ac084a704f00572a913275c41079907a737d9dd5
shadowcat143 <jameskuyt655@gmail.com>
2023-04-08 05:44:00 +0000

    meow

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const firebase = require("firebase/app");
-require("firebase/auth"); // Add additional Firebase services that you need
-require("firebase/database");
+import firebase from "firebase/app";
+import "firebase/auth";
+import * as firebaseui from "firebaseui";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBH3BN8w-NEtTglYYW3U3vE3cuudfb20h4",
@@ -10,7 +10,13 @@ const firebaseConfig = {
   messagingSenderId: "210724724949",
   appId: "1:210724724949:web:1341c491ce8b094041c25c"
 };
-
 firebase.initializeApp(firebaseConfig);
 
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
+ui.start("#firebaseui-auth-container", {
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  // Other configuration options
+});
