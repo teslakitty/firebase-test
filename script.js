fb94025276145ef4cdd365c887cc020a48a4c8c4
shadowcat143 <jameskuyt655@gmail.com>
2023-04-08 05:41:07 +0000

    meow

---
+++ script.js
@@ -0,0 +1,16 @@
+const firebase = require("firebase/app");
+require("firebase/auth"); // Add additional Firebase services that you need
+require("firebase/database");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBH3BN8w-NEtTglYYW3U3vE3cuudfb20h4",
+  authDomain: "test-21334.firebaseapp.com",
+  projectId: "test-21334",
+  storageBucket: "test-21334.appspot.com",
+  messagingSenderId: "210724724949",
+  appId: "1:210724724949:web:1341c491ce8b094041c25c"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
