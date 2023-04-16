e9f1c0ffa2bee487a8ce3432abdafb0021373792
James Kuyt <jkuyt39@gmail.com>
2023-04-16 14:37:32 +0000

    meow

---
+++ firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAosuUU_9XnZsDznwP-Z9kAEjgUH-fvZd4",
+    authDomain: "testmeow21.firebaseapp.com",
+    projectId: "testmeow21",
+    storageBucket: "testmeow21.appspot.com",
+    messagingSenderId: "423163438916",
+    appId: "1:423163438916:web:bb1b463a7f89b3da5d5911"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- script.js
+++ script.js
@@ -1,39 +1,6 @@
-import { initializeApp } from "firebase/compat/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/compat/auth";
-import { AuthUI } from "firebaseui";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBH3BN8w-NEtTglYYW3U3vE3cuudfb20h4",
-  authDomain: "test-21334.firebaseapp.com",
-  projectId: "test-21334",
-  storageBucket: "test-21334.appspot.com",
-  messagingSenderId: "210724724949",
-  appId: "1:210724724949:web:1341c491ce8b094041c25c"
-};
-
-// Initialize Firebase app
-const firebaseApp = initializeApp(firebaseConfig);
-
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
-
-const googleSignInButton = document.getElementById("google-signin-button");
-
-googleSignInButton.addEventListener("click", () => {
-  signInWithGoogle();
+var db = firebase.firestore();
+db.collection("users").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+  });
 });
-
-function signInWithGoogle() {
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user;
-      console.log(`Logged in as ${user.displayName}`);
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.error(`Error: ${errorMessage} (${errorCode})`);
-    });
-}
