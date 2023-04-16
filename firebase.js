afd81979cf1f1673178bb2c9d89aacc2a7c74882
James Kuyt <jkuyt39@gmail.com>
2023-04-16 14:53:40 +0000

    meow

--- firebase.js
+++ firebase.js
@@ -1,15 +1,16 @@
-import firebase from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js';
-import 'https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAosuUU_9XnZsDznwP-Z9kAEjgUH-fvZd4",
-    authDomain: "testmeow21.firebaseapp.com",
-    projectId: "testmeow21",
-    storageBucket: "testmeow21.appspot.com",
-    messagingSenderId: "423163438916",
-    appId: "1:423163438916:web:bb1b463a7f89b3da5d5911"
-  };
-  
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAosuUU_9XnZsDznwP-Z9kAEjgUH-fvZd4",
+  authDomain: "testmeow21.firebaseapp.com",
+  projectId: "testmeow21",
+  storageBucket: "testmeow21.appspot.com",
+  messagingSenderId: "423163438916",
+  appId: "1:423163438916:web:bb1b463a7f89b3da5d5911"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-import { initializeApp } from './firebase.js';
+const firebaseConfig = {
+  apiKey: "AIzaSyAosuUU_9XnZsDznwP-Z9kAEjgUH-fvZd4",
+  authDomain: "testmeow21.firebaseapp.com",
+  projectId: "testmeow21",
+  storageBucket: "testmeow21.appspot.com",
+  messagingSenderId: "423163438916",
+  appId: "1:423163438916:web:bb1b463a7f89b3da5d5911"
+};
 
-const app = initializeApp();
 
-app.then(() => {
-  const db = firebase.firestore();
-  db.collection("users").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-    });
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+// Add data to Firestore
+db.collection("users").add({
+  first: "Ada",
+  last: "Lovelace",
+  born: 1815
+})
+.then((docRef) => {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch((error) => {
+  console.error("Error adding document: ", error);
+});
+
+// Read data from Firestore
+db.collection("users").get().then((querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
   });
 });
